688eb67a0f17617c1dc9b585968e045811007409
stefanradev93 <stefan.radev93@gmail.com>
2025-08-27 08:04:21 -0400

    Update with inv kinematics and new ignores

--- case_study1/01_generate_data.py
+++ case_study1/01_generate_data.py
@@ -1,21 +1,33 @@
 import pickle
 
-from bayesflow.simulators import TwoMoons
+from bayesflow.simulators import TwoMoons, InverseKinematics
 
-NUM_TRAIN = 4096
+
+NUM_TRAIN = 2048
 NUM_VALIDATION = 512
 NUM_TEST = 512
 
 
 if __name__ == '__main__':
 
-    # Two moons benchmark
-    simulator = TwoMoons()
+    # Two moons low-dimensional benchmark
+    two_moons_simulator = TwoMoons()
 
-    two_moons_train_data = simulator.sample(NUM_TRAIN)
-    two_moons_validation_data = simulator.sample(NUM_VALIDATION)
+    two_moons_train_data = two_moons_simulator.sample(NUM_TRAIN)
+    two_moons_validation_data = two_moons_simulator.sample(NUM_VALIDATION)
 
     with open('./data/two_moons_train_data.pkl', 'wb') as f:
         pickle.dump(two_moons_train_data, f)
     with open('./data/two_moons_validation_data.pkl', 'wb') as f:
         pickle.dump(two_moons_validation_data, f)
+
+    # Inverse kinematics low-dimensional benchmark
+    inverse_kinematics_simulator = InverseKinematics()
+
+    inverse_kinematics_train_data = inverse_kinematics_simulator.sample(NUM_TRAIN)
+    inverse_kinematics_validation_data = inverse_kinematics_simulator.sample(NUM_VALIDATION)
+
+    with open('./data/inverse_kinematics_train_data.pkl', 'wb') as f:
+        pickle.dump(inverse_kinematics_train_data, f)
+    with open('./data/inverse_kinematics_validation_data.pkl', 'wb') as f:
+        pickle.dump(inverse_kinematics_validation_data, f)
--- case_study1/02_train_models.py
+++ case_study1/02_train_models.py
@@ -1,12 +1,14 @@
 import pickle
+import numpy as np
 
 import bayesflow as bf
 
 from keras.utils import clear_session
 
-EPOCHS = 1
-BATCH_SIZE = 128
 
+EPOCHS = 100
+BATCH_SIZE = 128
+NUM_SAMPLES_INFERENCE = 1000
 MODELS = {
         "flow_matching": (bf.networks.FlowMatching, {}),
         "ot_flow_matching": (bf.networks.FlowMatching, {"use_optimal_transport": True}),
@@ -30,12 +32,12 @@ MODELS = {
             "prediction_type": "noise"}),
     }
 
+DATASETS = ["two_moons", "inverse_kinematics"]
 
-def train_model(model_name, conf_tuple, data):
+def train_model(model_name, dataset_name, conf_tuple, data):
 
     adapter = (
         bf.adapters.Adapter()
-        .convert_dtype("float64", "float32")
         .rename("parameters", "inference_variables")
         .rename("observables", "inference_conditions")
     )
@@ -45,7 +47,7 @@ def train_model(model_name, conf_tuple, data):
     workflow = bf.workflows.BasicWorkflow(
         adapter=adapter,
         inference_network=inference_net,
-        checkpoint_filepath=f"checkpoints/{model_name}",
+        checkpoint_filepath=f"checkpoints/{model_name}_{dataset_name}",
         standardize="all"
     )
 
@@ -56,18 +58,28 @@ def train_model(model_name, conf_tuple, data):
         validation_data=data["validation"]
     )
 
+    if dataset_name == "two_moons":
+        # Posterior samples on, as typical in SBI (e.g., https://arxiv.org/abs/1905.07488)
+        obs = np.zeros((1, 2), dtype=np.float32)
+        
+    elif dataset_name == "inverse_kinematics":
+        # Posterior samples for position (0, 1.5) from https://arxiv.org/abs/2101.10763
+        obs = np.array([[0, 1.5]], dtype=np.float32)
+
+    samples = workflow.sample(conditions={"observables": obs}, num_samples=NUM_SAMPLES_INFERENCE)
+    np.save(f"samples/{model_name}_{dataset_name}.npy", samples["parameters"].squeeze())
+
+if __name__ == "__main__":
     
+    for dataset in DATASETS:
 
+        data_dict = {
+            "train": pickle.load(open(f"data/{dataset}_train_data.pkl", "rb")),
+            "validation": pickle.load(open(f"data/{dataset}_validation_data.pkl", "rb")),
+        }
 
-if __name__ == '__main__':
+        for model_name, conf_tuple in MODELS.items():
+            bf.utils.logging.info(f"Training {model_name} on {dataset}")
 
-    data_dict = {
-        "train": pickle.load(open("data/two_moons_train_data.pkl", "rb")),
-        "validation": pickle.load(open("data/two_moons_validation_data.pkl", "rb")),
-    }
-
-    for model_name, conf_tuple in MODELS.items():
-
-        bf.utils.logging.info(f"Training {model_name}")
-        train_model(model_name, conf_tuple, data_dict)
-        clear_session()
+            train_model(model_name, dataset, conf_tuple, data_dict)
+            clear_session()
