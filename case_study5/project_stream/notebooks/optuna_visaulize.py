713672b66f915b54844b610d9ee292f971c4644e
vepe99 <viterbogiuseppe1999@libero.it>
2026-03-11 11:40:04 +0100

    running hyperparam optimization for local model

--- case_study5/project_stream/notebooks/optuna_visaulize.py
+++ case_study5/project_stream/notebooks/optuna_visaulize.py
@@ -1,39 +1,46 @@
-import marimo
+# import marimo
 
-__generated_with = "0.19.5"
-app = marimo.App()
+# __generated_with = "0.19.5"
+# app = marimo.App()
 
 
-@app.cell
-def _():
+# @app.cell
+# def _():
+def main():
     # For multi-objective studies
+    import matplotlib.pyplot as plt
     import optuna
 
     from optuna.storages import JournalStorage, JournalFileStorage
 
     from optuna.visualization import plot_pareto_front, plot_param_importances
-    storage = JournalStorage(JournalFileStorage("../data/hyperparameter_tuning/optuna_diffusionmodel.log"))
+    storage = JournalStorage(JournalFileStorage("../data/hyperparameter_tuning/optuna_diffusionmodel_local.log"))
     study = optuna.load_study(
         # study_name="study_CompositionalDiffusionModel",
-        study_name='study_DiffusionModel',
+        study_name='study_DiffusionMode_local',
         storage=storage,
     )
     fig = plot_pareto_front(study, target_names=["RMSE", "Calibration Error"])
+    plt.savefig('plots_optuna/pareto_front.png')
     fig.show()
 
     fig = plot_param_importances(study, target=lambda t: t.values[0], target_name="RMSE")
+    plt.savefig('plots_optuna/param_importances_rmse.png')
     fig.show()
 
     fig = plot_param_importances(study, target=lambda t: t.values[1], target_name="Calibration Error")
+    plt.savefig('plots_optuna/param_importances_calibration_error.png')
     fig.show()
 
     return
 
 
-@app.cell
-def _():
-    return
+# @app.cell
+# def _():
+#     return
 
 
+# if __name__ == "__main__":
+#     app.run()
 if __name__ == "__main__":
-    app.run()
+    main()
